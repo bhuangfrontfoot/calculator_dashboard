5589a7d5981abe9f643d1f66fcd951dcdd22e063
Mehdi Valikhani <valikhani@sirangweb.com>
2015-03-31 16:08:40 +1100

    Jackpot added

---
+++ jobs/jackpot.rb
@@ -0,0 +1,11 @@
+send_event('jackpotchart', points: [
+      ['Pools', 'Total'],
+      ['Win',  40920.60],
+      ['Exacta',  3103.34],
+      ['First Four',  14143.70],
+      ['Place', 18970.40],
+      ['Duet', 831.54],
+      ['Running Double', 2187.50],
+      ['Quinella', 8097.64],
+      ['Trifecta', 21394.60]
+  ])
