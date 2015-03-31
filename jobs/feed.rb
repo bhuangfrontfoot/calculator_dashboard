887db9c2cef4b785fe80dbf9975fcf30c187e953
Brian Huang <brihuang@publicisgroupe.net>
2015-03-31 15:09:21 +1100

    Add youtube and info feed job

---
+++ jobs/feed.rb
@@ -0,0 +1,17 @@
+feed_messages = [
+ 'Tabcorp grows Q1 revenue 2.7%',
+ 'Tatts fails to win Victorian monitoring license',
+ 'Tabcorp bets on Telstra in big telecom deal',
+ 'Tabcorp bets on Cloud, iPad on Melbourne Cup day',
+ 'Two Way adds fixed odds betting to TAB ACTIVE'
+]
+
+SHOW_SIZE = 3
+
+pushed_feeds = []
+
+SCHEDULER.every '10s' do
+  pushed_feeds.push( feed_messages.sample )
+
+  send_event('feed', {items: pushed_feeds.last(SHOW_SIZE)})
+end
